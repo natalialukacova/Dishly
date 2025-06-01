799019b3470caccfa25bbad05b72639eef150bca
natalialukacova <natalialukacova11@gmail.com>
2025-06-02 01:26:12 +0200

    Fix aiBaseUrl

--- mobile/lib/constants.dart
+++ mobile/lib/constants.dart
@@ -1,2 +1,2 @@
 const String backendBaseUrl = 'https://dishly-production.up.railway.app';
-const String aiBaseUrl = 'https://dishly-ai.up.railway.app';
+const String aiBaseUrl = 'https://dishly-production-05ca.up.railway.app';
