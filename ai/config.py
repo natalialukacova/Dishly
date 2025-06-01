1748e09f55901efbacdaca8364e5d50370283f17
natalialukacova <natalialukacova11@gmail.com>
2025-06-01 23:22:24 +0200

    Add python docker file

--- ai/config.py
+++ ai/config.py
@@ -1,5 +1,6 @@
-﻿OLLAMA_MODEL_NAME = "llama3"
+﻿import os
 
-MONGO_URI = "mongodb://localhost:27017"
-MONGO_DB_NAME = "dishly"
-MONGO_COLLECTION = "chat_memory"
+OLLAMA_MODEL_NAME = os.getenv("OLLAMA_MODEL_NAME", "llama3")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "dishly")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "chat_memory")
--- ai/main.py
+++ ai/main.py
@@ -3,3 +3,7 @@ from api_routes import router as app_router
 
 app = FastAPI()
 app.include_router(app_router)
+
+@app.get("/")
+def root():
+    return {"message": "AI backend is running"}
